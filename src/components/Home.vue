9bcdf005e1e1413b911dd6da121ec622f5ae7409
Martin <mr.mmaurer+work@gmail.com>
2020-04-12 19:05:52 +0200

    Added test slider

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,12 +3,38 @@
     <ion-header>
       <ion-toolbar color="primary">
         <ion-title>
-          Header
+          <h1>Header</h1>
         </ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content padding>
-      Content
+      <ion-slides
+        pager="true"
+        :options="slideOptions"
+      >
+        <ion-slide>
+          <div class="slide-content">
+            <h2 class="slide-title">
+              Carbon
+            </h2>
+            <ion-img
+              class="slide-image"
+              src="carbon.svg"
+            />
+          </div>
+        </ion-slide>
+        <ion-slide>
+          <div class="slide-content">
+            <h2 class="slide-title">
+              Astatine
+            </h2>
+            <ion-img
+              class="slide-image"
+              src="astatine.svg"
+            />
+          </div>
+        </ion-slide>
+      </ion-slides>
     </ion-content>
     <ion-footer>
       <ion-toolbar color="secondary">
@@ -23,9 +49,28 @@ export default {
   name: 'Home',
   props: {
   },
+  data() {
+    return {
+      slideOptions: {
+        initialSlide: 1,
+        speed: 400,
+      },
+    };
+  },
 };
+
 </script>
 
 <style scoped lang="scss">
+.slide-content {
+  display: block;
+}
 
+.slide-title {
+
+}
+.slide-image {
+  max-width: 75%;
+  margin: auto;
+}
 </style>
