3bb3a94a8627a62ecdb788e236602768b36e9d02
Martin <mr.mmaurer+work@gmail.com>
2020-04-13 01:05:33 +0200

    Added v-model workaround and dummy modulus check

--- src/components/Banking.vue
+++ src/components/Banking.vue
@@ -24,6 +24,8 @@
             maxlength="8"
             type="number"
             clear-input
+            :value="accountNumber"
+            @input="accountNumber = $event.target.value"
           />
         </ion-item>
 
@@ -37,12 +39,18 @@
             maxlength="6"
             type="number"
             clear-input
+            :value="sortCode"
+            @input="sortCode = $event.target.value"
           />
         </ion-item>
 
         <ion-button type="submit">
           Submit
         </ion-button>
+
+        <p v-if="showModulusCheckResults">
+          Modulus check has been successful.
+        </p>
       </form>
     </ion-content>
     <ion-footer>
@@ -54,18 +62,60 @@
 </template>
 
 <script>
+import { addIcons } from 'ionicons';
+import { checkmarkDoneCircleOutline } from 'ionicons/icons';
+
+addIcons({
+  'checkmark-circle-outline': checkmarkDoneCircleOutline,
+});
+
 export default {
   name: 'Banking',
   props: {
   },
   data() {
     return {
-
+      accountNumber: '',
+      sortCode: '',
+      showModulusCheckResults: false,
+      modulusCheck: false,
     };
   },
   methods: {
-    handleSubmit() {
-      console.log('submit');
+    async handleSubmit() {
+      const isModulusCheckSuccessful = await this.getModulusCheckResults();
+
+      if (isModulusCheckSuccessful) {
+        this.showModulusCheckResults = true;
+      }
+    },
+    async getModulusCheckResults() {
+      try {
+        const modulusCheckResults = new Promise((resolve) => {
+          const accountNumberContainsNumbers = new RegExp(/^\d+$/).test(this.accountNumber);
+          const accountNumberHasCorrectLength = this.accountNumber.length === 8;
+
+          const sortCodeContainsNumbers = new RegExp(/^\d+$/).test(this.sortCode);
+          const sortCodeHasCorrectLength = this.sortCode.length === 6;
+
+          const checks = [
+            accountNumberContainsNumbers,
+            accountNumberHasCorrectLength,
+            sortCodeContainsNumbers,
+            sortCodeHasCorrectLength,
+          ];
+
+          const results = checks.every((check) => check);
+
+          setTimeout(() => {
+            resolve(results);
+          }, 2000);
+        });
+
+        return modulusCheckResults;
+      } catch (error) {
+        return error;
+      }
     },
   },
 };
